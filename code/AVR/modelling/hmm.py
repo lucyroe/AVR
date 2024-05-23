675ea9ea8d962ca51d34eaf8715f3a6e75efa3d6
lucyroe <lucy.roellecke@fu-berlin.de>
2024-05-23 17:18:55 +0200

    added script for preprocessing of nevro annotation data + updated hmm script

--- code/AVR/modelling/hmm.py
+++ code/AVR/modelling/hmm.py
@@ -1,7 +1,7 @@
 """
-Script to perform a Hidden Markov Model (HMM) analysis on ECG and EEG Data.
+Script to perform a Hidden Markov Model (HMM) analysis on Annotation, ECG and EEG Data.
 
-Inputs: Preprocessed ECG and EEG data.
+Inputs: Preprocessed Annotation, ECG and EEG data.
 
 Outputs: TODO: define
 
@@ -11,98 +11,190 @@ Functions:
 Steps:
 1. 
 
-Required packages: mne, neurokit
+Required packages: 
 
 Author: Lucy Roellecke
 Contact: lucy.roellecke@fu-berlin.de
-Last update: May 22th, 2024
+Last update: May 23rd, 2024
 """
 
 # %% Import
 import os
 from pathlib import Path
+import glob
 
 import matplotlib.pyplot as plt
 from hmmlearn.hmm import GaussianHMM
-import mne
-import neurokit2 as nk
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
 # %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
-physiopath = "/Users/Lucy/Documents/Berlin/FU/MCNB/Praktikum/MPI_MBE/AVR/data/NeVRo/preprocessed/physiological/"
+datapath = "/Users/Lucy/Documents/Berlin/FU/MCNB/Praktikum/MPI_MBE/AVR/data/NeVRo/preprocessed/"
 
+modalities = ["physiological", "annotations"]
+physiological_measures = ["ECG"]
+#, "EEG"
+
+# if debug is True, only one subject will be analyzed
 debug = True
 
+# Number of states (low and high HR) = hyperparameter that needs to be chosen
+number_of_states = 2
+
+# Number of iterations for training the HMM
+iterations = 1000
+
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+def create_model(data, num_states, iterations=1000):
+    """
+    Create and train a Hidden Markov Model (HMM) on the given data.
+    Then predicts the hidden states.
+
+    Args:
+        data (np.array): The data to train the HMM on.
+        num_states (int): The number of states of the HMM.
+        iterations (int): The number of iterations to train the HMM (defaults to 1000).
+
+    Returns:
+        model (GaussianHMM): The trained HMM model.
+        hidden_states (np.array): The predicted hidden states.
+    """
+    model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=iterations)
+    model.fit(data)
+    hidden_states = model.predict(data)
+
+    return model, hidden_states
+
+def plot_hidden_states(data, hidden_states, num_states, title):
+    """
+    Plot the data with the hidden states marked in color vertically.
+
+    Args:
+        data (np.array): The data to plot.
+        hidden_states (np.array): The hidden states of the data.
+        num_states (int): The number of states.
+        title (str): The title of the plot.
+    
+    Returns:
+        figure: The plot.
+    """
+    figure = plt.figure()
+    plt.plot(data)
+    plt.title(title)
+    for i in range(num_states):
+        plt.fill_between(np.arange(len(data)), 0, 200, where=(hidden_states == i), color="C" + str(i), alpha=0.3)
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel(str(data))
+
+    plt.show()
+
+    return figure
 
 # %% __main__  >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 # %% STEP 1. GET DATA
 if __name__ == "__main__":
-    # get the file path for each file of the dataset
-    file_list = os.listdir(physiopath)
+    for modality in modalities:
+        # get right file path for the dataset
+        filepath = Path(datapath) / modality
+        if modality == "physiological":
+            for physiological_measure in physiological_measures:
+                # list all files in the physiological folder corresponding to the physiological measure
+                pattern_measure = f"*_{physiological_measure}_preprocessed.tsv"
+                file_list = glob.fnmatch.filter(os.listdir(filepath), pattern_measure)
 
-    # deletes all hidden files from the list
-    file_list = [file for file in file_list if not file.startswith(".")]
+                # delete all files that are not subject files
+                file_list = [file for file in file_list if file.startswith("sub")]
 
-    # delete all files that are not subject files
-    file_list = [file for file in file_list if file.startswith("sub")]
+                # sort the list in ascending order
+                file_list = sorted(file_list)
 
-    # sort the list in ascending order
-    file_list = sorted(file_list)
+                # get participant list from the file list
+                subject_list = [file.split("_")[1][0:3] for file in file_list]
 
-    # get participant list from the file list
-    subject_list = [file.split("_")[1][0:3] for file in file_list]
+                # only analyze one subject if debug is True
+                if debug:
+                    subject_list = [subject_list[0]]
 
-    # only analyze one subject if debug is True
-    if debug:
-        subject_list = [subject_list[1]]
+                # Loop over all subjects
+                for subject_index, subject in enumerate(subject_list):
+                    print(f"Processing subject {subject_index+1} (ID {subject}) of " + str(len(subject_list)) + "...")
 
-    # Loop over all subjects
-    for subject_index, subject in enumerate(subject_list):
-        print(f"Processing subject {subject_index+1} (ID {subject}) of " + str(len(subject_list)) + "...")
+                    # Get the right file for the subject
+                    pattern_subject = f"sub_{subject}_mov_Space_{physiological_measure}_preprocessed.tsv"
+                    subject_file = glob.fnmatch.filter(file_list, pattern_subject)[0]
 
-        # Read in data
-        subject_data = pd.read_csv(physiopath + "sub_" + subject + "_mov_Space_ECG_preprocessed.tsv", sep="\t")
+                    # Read in data
+                    subject_data = pd.read_csv(filepath / subject_file, sep="\t")
 
-        # Get HR
-        hr = subject_data["HR"].dropna().values.reshape(-1, 1)
+                    # Get measure
+                    if physiological_measure == "ECG":
+                        data = subject_data["HR"].dropna().values.reshape(-1, 1)
+                    else:
+                        ...
 
-        # Plot HR
-        plt.figure()
-        plt.plot(hr)
+                    # Plot data
+                    plt.figure()
+                    plt.plot(data)
 
-        # %% STEP 2. HMM
+        else:
+            # list all files in the annotations folder
+            file_list = os.listdir(filepath)
 
-        # Create and train the Hidden Markov Model
-        print("\nCreating and training the Hidden Markov Model...")
-        num_states = 2  # Number of states (low and high HR) = hyperparameter that needs to be chosen
-        model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000)
-        model.fit(hr)
+            # deletes all hidden files from the list
+            file_list = [file for file in file_list if not file.startswith(".")]
 
-        # Predict the hidden states of HMM
-        hidden_states = model.predict(hr)
+            # delete all files that are not subject files
+            file_list = [file for file in file_list if file.startswith("sub")]
 
-        print("\nMeans and variances of hidden states:")
-        for i in range(model.n_components):
-            print("Hidden state", i+1)
-            print("Mean = ", round(model.means_[i][0], 3))
-            print("Variance = ", np.diag(model.covars_[i][0], 3))
+            # sort the list in ascending order
+            file_list = sorted(file_list)
 
-        # Plot the hidden states
-        plt.figure()
-        plt.plot(hidden_states)
-        plt.title("Hidden States")
+            # get participant list from the file list
+            subject_list = [file.split("_")[1][0:3] for file in file_list]
 
-        # Plot the heart rate with the hidden states marked in color vertically
-        plt.figure()
-        plt.plot(hr)
-        plt.title("Heart Rate")
-        for i in range(num_states):
-            plt.fill_between(np.arange(len(hr)), 0, 200, where=(hidden_states == i), color="C" + str(i), alpha=0.3)
+            # only analyze one subject if debug is True
+            if debug:
+                subject_list = [subject_list[0]]
 
+            # Loop over all subjects
+            for subject_index, subject in enumerate(subject_list):
+                print(f"Processing subject {subject_index+1} (ID {subject}) of " + str(len(subject_list)) + "...")
+
+                # Get the right file for the subject
+                pattern_subject = f"sub_{subject}_mov_space_arousal_preprocessed.tsv"
+                subject_file = glob.fnmatch.filter(file_list, pattern_subject)[0]
+
+                # Read in data
+                subject_data = pd.read_csv(filepath / subject_file, sep="\t")
+
+                # Get the annotations
+                arousal = subject_data["cr_a"].dropna().values.reshape(-1, 1)
+
+                # Plot the annotations
+                plt.figure()
+                plt.plot(arousal)
+                plt.ylim(-1, 1)
+
+
+                # %% STEP 2. HMM
+
+                # Create and train the Hidden Markov Model
+                print("\nCreating and training the Hidden Markov Model...")
+
+                model_arousal, hidden_states_arousal = create_model(arousal, number_of_states, iterations)
+
+                print("\nMeans and variances of hidden states for arousal:")
+                for i in range(model_arousal.n_components):
+                    print("Hidden state", i+1)
+                    print("Mean = ", round(model_arousal.means_[i][0], 3))
+                    print("Variance = ", np.diag(model_arousal.covars_[i][0], 3))
+                
+                # Plot the annotations with the hidden states marked in color vertically
+                plot_hidden_states(arousal, hidden_states_arousal, number_of_states, "Arousal with Hidden States")
+                # TODO: this does not work atm
 
 
 # %%
---
+++ code/AVR/preprocessing/annotation/preprocessing_annotation_nevro.py
@@ -0,0 +1,166 @@
+"""
+Script to preprocess annotation data for a given dataset (atm: NeVRo).
+
+Inputs: Raw annotation data
+
+Outputs: Preprocessed annotation data as tsv file (both for subjects separrately and averaged over all subjects)
+
+Steps:
+1. LOAD DATA
+
+2. PREPROCESS DATA
+    2a. Formatting data (column names, etc.)
+    2b. Rescale arousal ratings from 0 to 100 range to -1 to 1 range
+    2c. Round arousal ratings to 2 decimal places
+    2d. Save preprocessed data
+
+3. AVERAGE OVER ALL PARTICIPANTS
+    3a. Concatenate data of all participants into one dataframe
+    3b. Save dataframe with all participants (all_..._arousal_preprocessed.tsv)
+    3c. Calculate mean arousal data
+    3d. Save dataframe with mean arousal data (avg_..._arousal_preprocessed.tsv)
+
+Author: Lucy Roellecke
+Contact: lucy.roellecke@fu-berlin.de
+Last update: May 23rd, 2024
+"""
+
+# %% Import
+import glob
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+datapath = "/Volumes/LUCY_MEMORY/NeVRo/Data/ratings/continuous/not_z_scored/"
+preprocessed_path = "/Users/Lucy/Documents/Berlin/FU/MCNB/Praktikum/MPI_MBE/AVR/data/NeVRo/preprocessed/annotations/"
+
+# create the preprocessed folder if it does not exist
+Path(preprocessed_path).mkdir(parents=True, exist_ok=True)
+
+# define conditions to analyze (mov = movement, nomov = no movement)
+conditions = ["mov"]
+# "nomov"
+
+# sections to analyze
+sections = ["space"]
+# "break", "anden"  # noqa: ERA001
+
+# length of these sections in seconds
+# the sections space and anden were already trimmed by 2.5s in the beginning and in the end = 5s in total
+# that is why the lengths defined here are 5s shorter than the lengths of the physiological datastreams
+section_lengths = {"space": 148, "break": 30, "anden": 92}
+
+# sampling rates for annotation data in Hz
+sampling_rate = 1
+
+# only analyze one subject when debug mode is on
+debug = False
+
+# %% __main__  >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+
+# %% STEP 1. LOAD DATA
+if __name__ == "__main__":
+    for condition in conditions:
+        for section in sections:
+            # get right file path for the dataset
+            filepath = Path(datapath) / condition / section
+
+            # get the file path for each file of the dataset
+            file_list = os.listdir(filepath)
+
+            # deletes all hidden files from the list
+            file_list = [file for file in file_list if not file.startswith(".")]
+
+            # delete all files that are not .txt files
+            file_list = [file for file in file_list if file.endswith(".txt")]
+
+            # sort the list in ascending order
+            file_list = sorted(file_list)
+
+            # get participant list from the file list
+            subject_list = [file.split("_")[1][1:3] for file in file_list]
+
+            # only analyze one subject if debug is True
+            if debug:
+                subject_list = [subject_list[0]]
+
+            # Create empty list for data of all participants
+            list_data_all = []
+
+            # Loop over all subjects
+            for subject_index, subject in enumerate(subject_list):
+                print(f"Processing subject {subject_index+1} (ID {subject}) of " + str(len(subject_list)) + "...")
+
+                # Get the right file for the subject
+                pattern = f"NVR_S{subject}_run_?_{section}_rat_z.txt"
+
+                # Find the file in file_list that matches the pattern
+                subject_file = glob.fnmatch.filter(file_list, pattern)[0]
+
+                # Read in data
+                subject_data = pd.read_csv(Path(filepath) / subject_file, sep=",", header=None)
+
+                # %% STEP 2. PREPROCESS DATA
+                # Formatting data
+                # Delete third column
+                subject_data = subject_data.drop(columns=2)
+                # Rename columns
+                subject_data.columns = ["time", "cr_a"]
+                # Add column with subject ID as the first column
+                subject_data.insert(0, "sj_id", subject)
+
+                # Rescale arousal ratings from 0 to 100 range to -1 to 1 range
+                subject_data["cr_a"] = (subject_data["cr_a"] - 50) / 50
+
+                # Round arousal ratings to 2 decimal places
+                subject_data["cr_a"] = subject_data["cr_a"].round(2)
+
+                # Save preprocessed data
+                subject_data.to_csv(
+                    preprocessed_path + f"sub_{subject}_{condition}_{section}_arousal_preprocessed.tsv",
+                    sep="\t",
+                    index=False,
+                )
+                # Data is saved as a tsv file with the following columns (in that order):
+                # "sj_id": subject ID
+                # "time": time (seconds, s), sampling rate = 1 Hz, starts at 1
+                # "cr_a": arousal rating (-1-1)
+
+                # Add participant's data to dataframe for all participants
+                list_data_all.append(subject_data)
+
+            # %% STEP 3. AVERAGE OVER ALL PARTICIPANTS
+
+            # Concatenate data of all participants into one dataframe
+            data_all = pd.concat(list_data_all, ignore_index=True)
+
+            # Save dataframe with all participants in tsv file
+            data_all.to_csv(
+                preprocessed_path + f"all_{condition}_{section}_arousal_preprocessed.tsv", sep="\t", index=False
+            )
+
+            # Drop column with subject number
+            data_all = data_all.drop(columns=["sj_id"])
+            # Calculate mean arousal data
+            data_mean = data_all.groupby("time").mean()
+            # Add time column as first column
+            data_mean.insert(0, "time", data_mean.index)
+
+            # Save dataframe with mean arousal data in tsv file
+            data_mean.to_csv(
+                preprocessed_path + f"avg_{condition}_{section}_arousal_preprocessed.tsv", sep="\t", index=False
+            )
+
+            # Plot mean arousal data
+            plt.figure()
+            sns.lineplot(x="time", y="cr_a", data=data_mean)
+            plt.title(f"Mean arousal ratings during {section} in {condition} condition")
+            plt.xlabel("Time (s)")
+            plt.ylabel("Arousal rating")
+            plt.ylim(-1, 1)
+            plt.show()
+# %%
